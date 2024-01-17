d3e544a49b247a430a4e5f44e4d26df49f955eb7
Sam Rutcovschi <crutcovschi@gmail.com>
2024-01-17 19:12:00 +0200

    add entities

---
+++ src/rutcovschi/intercon-be/Intercon.Domain/Business.cs
@@ -0,0 +1,15 @@
+﻿namespace Intercon.Domain;
+
+//public class Business
+//{
+//    public Guid Id { get; set; }
+//    public string Title {  get; set; }
+//    public string ShortDescription { get; set; }
+//    public string FullDescription { get; set; }
+//    public float RatingAvg { get; set; }
+//    public string Image {  get; set; } //to do: trebuie de clarificat data type
+//    public string Address { get; set; }
+//    public uint ReviewsCount { get; set; }
+//    //public List<int, int> AllReviewsCount { get; set; }
+//    public Review Reviews { get; set; }
+//}
---
+++ src/rutcovschi/intercon-be/Intercon.Domain/Review.cs
@@ -0,0 +1,38 @@
+﻿namespace Intercon.Domain;
+
+
+public class Business
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string ShortDescription { get; set; }
+    public string FullDescription { get; set; }
+    public float RatingAvg { get; set; }
+    public string Image { get; set; } //to do: trebuie de clarificat data type
+    public string Address { get; set; }
+    public uint ReviewsCount { get; set; }
+    //public List<int, int> AllReviewsCount { get; set; }
+    public Review Reviews { get; set; }
+}
+
+public class Review
+{
+    public User Author { get; set; }
+    public string AuthorFullName {
+        get => $"{Author.FirstName} {Author.LastName}";
+        private set { }
+    }
+    public string AuthorEmail
+    {
+        get => Author.Email;
+        private set { }
+    }
+}
+
+
+public class User
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string  Email {  get; set; } = string.Empty;
+}
