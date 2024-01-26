1f7d3fb6dd388ceef4cb3eecf6d17cee8d3b9fe0
Sam Rutcovschi <crutcovschi@gmail.com>
2024-01-26 18:15:05 +0200

    add initial UserController

---
+++ src/rutcovschi/intercon-be/Intercon.Application/DataTransferObjects/User/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Intercon.Application.DataTransferObjects.User;
+
+public class UserDto
+{
+    public int Id { get; init; }
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+}
---
+++ src/rutcovschi/intercon-be/Intercon.Presentation/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Intercon.Application.DataTransferObjects.User;
+using Intercon.Application.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intercon.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetUser(int id)
+    {
+        return Ok(await _userService.GetUser(id));
+    }
+
+    [HttpGet()]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+    {
+        return Ok(await _userService.GetUsers());
+    }
+
+    [HttpPost()]
+    public async Task<ActionResult> CreateUsers([FromBody] UserDto userToAdd)
+    {
+        return Ok(await _userService.CreateUser(userToAdd));
+    }   
+
+    [HttpPut()]
+    public async Task<ActionResult> EditUser([FromBody] UserDto userToEdit)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<UserDto>> DeleteUser([FromRoute] int id)
+    {
+        return Ok(await _userService.DeleteUser(id));
+    }
+}
