92510d98632bcecd8c22009a12ead12c3777ace2
1Leomas <81852464+1Leomas@users.noreply.github.com>
2024-04-10 14:29:05 +0200

    feat: update ImagesSeeder

--- src/rutcovschi/intercon-be/Intercon.Infrastructure/Persistence/DataSeeder/Seeds/ImagesSeed.cs
+++ src/rutcovschi/intercon-be/Intercon.Infrastructure/Persistence/DataSeeder/Seeds/ImagesSeed.cs
@@ -35,6 +35,10 @@ public class ImagesSeed
 
         images.AddRange(SeedLinellaLogos());
         images.AddRange(SeedGranierLogos());
+        images.AddRange(SeedBigSportGymLogos());
+        images.AddRange(SeedJungleFitnessLogos());
+        images.AddRange(SeedMcDonaldsLogos());
+        images.AddRange(SeedDarwinLogos());
 
         return images;
     }
@@ -70,5 +74,64 @@ public class ImagesSeed
         return images;
     }
 
+    private static List<FileData> SeedBigSportGymLogos() // 29-33
+    {
+        var images = new List<FileData>();
 
+        for (int i = 29; i <= 33; i++)
+        {
+            images.Add(new FileData
+            {
+                Id = i,
+                Path = "https://interconblobstorage.blob.core.windows.net/images/379e13fc-f653-46b9-83ec-59b5264fe187.png"
+            });
+        }
+
+        return images;
+    }
+
+    private static List<FileData> SeedJungleFitnessLogos() // 34-34
+    {
+        var images = new List<FileData>();
+
+        for (int i = 34; i <= 34; i++)
+        {
+            images.Add(new FileData
+            {
+                Id = i,
+                Path = "https://interconblobstorage.blob.core.windows.net/images/379e13fc-f653-46b9-83ec-59b5264fe188.jpg"
+            });
+        }
+        return images;
+    }
+
+    private static List<FileData> SeedMcDonaldsLogos() // 35-43
+    {
+        var images = new List<FileData>();
+
+        for (int i = 35; i <= 43; i++)
+        {
+            images.Add(new FileData
+            {
+                Id = i,
+                Path = "https://interconblobstorage.blob.core.windows.net/images/379e13fc-f653-46b9-83ec-59b5264fe189.png"
+            });
+        }
+        return images;
+    }
+
+    private static List<FileData> SeedDarwinLogos() // 44-50
+    {
+        var images = new List<FileData>();
+
+        for (int i = 44; i <= 50; i++)
+        {
+            images.Add(new FileData
+            {
+                Id = i,
+                Path = "https://interconblobstorage.blob.core.windows.net/images/379e13fc-f653-46b9-83ec-59b5264fe186.png"
+            });
+        }
+        return images;
+    }
 }
