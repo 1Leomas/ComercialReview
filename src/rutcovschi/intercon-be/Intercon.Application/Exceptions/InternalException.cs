a1091d861ff0e896cabf05a8daab808f6a9e8c02
Sam Rutcovschi <crutcovschi@gmail.com>
2024-04-14 18:34:27 +0300

    feat: custom exceptions

---
+++ src/rutcovschi/intercon-be/Intercon.Application/Exceptions/InternalException.cs
@@ -0,0 +1,8 @@
+﻿namespace Intercon.Application.Exceptions;
+
+public class InternalException : BaseException
+{
+    public InternalException() : base(("Operation failed due to an internal server error. Please try again later."))
+    {
+    }
+}
